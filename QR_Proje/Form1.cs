47d776bdb46dcc86ef7ea652cd2bd09b45efddab
mertcankomurcu <mertcankomurcu@outlook.com.tr>
2024-02-28 22:45:17 +0300

    Proje dosyası ekle.

---
+++ QR_Proje/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace QR_Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeEncoder enc = new QRCodeEncoder();
+            pictureBox1.Image = enc.Encode(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            QRCodeDecoder dnc=new QRCodeDecoder();
+            textBox1.Text = dnc.Decode(new QRCodeBitmapImage(pictureBox1.Image as Bitmap));
+        }
+    }
+}
